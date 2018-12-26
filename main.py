e83afb7b14ccb464671400cde89dbde942801b4e
unknown <jenario171@yahoo.com>
2018-12-26 01:43:09 -0500

    base app

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 #from mbientlab.metawear import MetaWear, libmetawear
 #from mbientlab.metawear.cbindings import *
@@ -23,14 +24,14 @@ from time import sleep
 from threading import Event
 from os import listdir
 import os
-os.environ['KIVY_GL_BACKEND'] = 'gl'
-import sys
+#os.environ['KIVY_GL_BACKEND'] = 'gl'
+#import sys
 
 kv_path = './kv/'
 for kv in listdir(kv_path):
     Builder.load_file(kv_path+kv)
 
-class DeviceButton(Button):
+class ActivityButton(Button):
     pass
 
 class SubtractButton(Button):
@@ -40,11 +41,13 @@ class MacInput(BoxLayout):
     pass
 
 
-class Container(GridLayout):
-    display = ObjectProperty()
-    
+class MainScreen(Screen):
     textinput = TextInput(text='Hello world')
-    
+    b = BoxLayout()
+    t = TextInput()
+    f = FloatLayout()
+    b.add_widget(f)
+    b.add_widget(t)
     def enter_mac(self):
         self.display.text = "Scanning"
         MacInput()
@@ -64,13 +67,13 @@ class Container(GridLayout):
 #        libmetawear.mbl_mw_led_write_pattern(device.board, byref(pattern), LedColor.GREEN)
 #        libmetawear.mbl_mw_led_play(device.board)
 
-
-
-    b = BoxLayout()
-    t = TextInput()
-    f = FloatLayout()
-    b.add_widget(f)
-    b.add_widget(t)
+class ActivitiesScreen(Screen):
+    pass
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MainScreen(name='main'))
+sm.add_widget(ActivitiesScreen(name='activities'))
+    
     
     
     
@@ -78,7 +81,7 @@ class MainApp(App):
     
     def build(self):
         self.title = 'Awesome app!!!'
-        return Container()
+        return sm
     
 if __name__ == "__main__":
     Config.set('graphics', 'fullscreen', '0')
